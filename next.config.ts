c6f0671f23c2f7516e3e32a6540279af71302aa5
Jacob Mathison <jacob@mathisonprojects.com>
2024-11-30 21:50:20 -0800

    fixing the out

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
-  target: "server",
+  distDir: 'out',
   reactStrictMode: true,
 };
 
